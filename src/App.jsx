10c212f1f40e1550f360e269aa103147e9910e63
henrichfin <vhinzhenrich@gmail.com>
2024-07-12 17:26:44 +0800

    message

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      
+      <h1>Miafe Coraza</h1>
+      <h2>Myrene Arsenal</h2>
+     
+      
+    </div>
+  )
+}
+
+export default App
